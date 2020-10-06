93a486034dacf3fe0166b43003d90d1c7aed892c
Christoffer Kronblad <crisse.kronblad@gmail.com>
2020-10-06 13:29:27 +0200

    modified

--- bullet.js
+++ bullet.js
@@ -10,16 +10,10 @@
                 app.stage.interactive = true;
             
 
-        //Player object
 
-        player = new PIXI.Sprite.from("images/monkey-1b.png");
-        player.anchor.set(0.5);
-        player.x = app.view.width / 2;
-        player.y = app.view.height / 2;
 
         
         
-        app.stage.addChild(player);
         
         // Movement
         
---
+++ load.js
@@ -0,0 +1,15 @@
+window.onload = function()
+{
+    app = new PIXI.Application({resizeTo:window, backgroundColor: 0xAAAAAA});
+    document.body.appendChild(app.view);
+
+
+    //Player object
+    
+    player = new PIXI.Sprite.from("images/monkey-1b.png");
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+    app.stage.addChild(player);
+
+}
