ee85c2a2c3bc21766b3ffa2344ab866bcc4dac87
Fanniebannie <31246570+Fanniebannie@users.noreply.github.com>
2020-10-06 13:32:03 +0200

    Uppdatearat

--- bullet.js
+++ bullet.js
@@ -11,11 +11,10 @@
             
 
         //Player object
-
         player = new PIXI.Sprite.from("images/monkey-1b.png");
         player.anchor.set(0.5);
         player.x = app.view.width / 2;
-        player.y = app.view.width / 1.5;
+        player.y = app.view.height / 2;
 
 
         app.stage.addChild(player);
